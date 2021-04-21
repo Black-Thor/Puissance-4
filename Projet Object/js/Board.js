92b729b733a20e2ce7d31c5f3d0c6ff3c6ed1767
Stephane Duboze <stephane857@live.fr>
2021-04-21 18:44:24 +0200

    rendering

--- Projet Object/js/Board.js
+++ Projet Object/js/Board.js
@@ -4,4 +4,35 @@ class Board {
         this.columns = 7;
         this.spaces = [];
     }
+
+      /** 
+     * Generates 2D array of spaces. 
+     * @return  {Array}     An array of space objects
+     */
+    
+       createSpace() {
+        const spaces = [] ; 
+        for (let x = 0; x < this.columns; x++) {
+            const column = [] ; 
+            for (let y = 0; y < this.rows; y++) {
+                const space = new Space(x,y) ;
+                column.push(space) ;  
+                
+            }
+            spaces.push(columns) ; 
+        }
+
+        return  this.spaces ; 
+    }
+    /** 
+     * Draws associated SVG spaces for all game spaces.
+     */
+    drawHTMLBoard() {
+        for (let column of this.spaces) {
+            for (let space of column) {
+                space.drawSVGSpace();
+            }
+        }
+    }
+
 }
--- Projet Object/js/Token.js
+++ Projet Object/js/Token.js
@@ -8,5 +8,17 @@ class Token {
     createTokens() {
         
     }
-    
+    get htmlToken() {
+        return document.getElementById(this.id);
+    }
+     /** 
+     * Draws new HTML token.
+     */
+      drawHTMLToken(){
+        const token = document.createElement('div');
+        document.getElementById('game-board-underlay').appendChild(token);
+        token.setAttribute('id', this.id);
+        token.setAttribute('class', 'token');
+        token.style.backgroundColor = this.owner.color;
+    }
 }
