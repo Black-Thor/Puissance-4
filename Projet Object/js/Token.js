e6e00e4b5978484497a4dd27577b81ab8c73d211
Stephane Duboze <stephane857@live.fr>
2021-04-22 21:48:29 +0200

    Interaction

--- Projet Object/js/Token.js
+++ Projet Object/js/Token.js
@@ -3,6 +3,7 @@ class Token {
         this.owner = owner ; 
         this.dropped = false ; 
         this.id = `token-${index}-${owner.id}`; //crée un token unique 
+        this.columnLocation = 0;
     }
     //methode 
     createTokens() {
@@ -21,4 +22,44 @@ class Token {
         token.setAttribute('class', 'token');
         token.style.backgroundColor = this.owner.color;
     }
+    /** 
+     * Gets left offset of html element.
+     * @return  {number}   Left offset of token object's htmlToken.
+     */
+    get offsetLeft() {
+        return this.htmlToken.offsetLeft;
+    }
+        /** 
+     * Moves html token one column to left.
+     */
+    moveLeft() {
+        if (this.columnLocation > 0 ) {
+            this.htmlToken.style.left = this.offsetLeft - 76 ; 
+            this.columnLocation -= 1;
+
+        }
+
+    }
+    /** 
+     * Moves html token one column to right.
+	 * @param 	{number}	columns - number of columns in the game board
+     */
+    moveRight() {
+        if (this.columnLocation < columns - 1) {
+            this.htmlToken.style.left = this.offsetLeft + 76;
+            this.columnLocation += 1;
+        }
+    }
+    /** 
+     * Drops html token into targeted board space.
+     * @param   {Object}   target - Targeted space for dropped token.
+     * @param   {function} reset  - The reset function to call after the drop animation has completed.
+     */
+    drop(target,reset  ) {
+        this.dropped = true;
+        $(this.htmlToken).animate({
+            top: (target.y * target.diameter)
+        }, 750, 'easeOutBounce', reset);
+    }
+
 }
--- Projet Object/js/app.js
+++ Projet Object/js/app.js
@@ -8,3 +8,10 @@ const game  = new Game() ;
     this.style.display = 'none';
     document.getElementById('play-area').style.opacity = '1';
 });
+
+/** 
+ * Listen for keyboard presses
+ */
+ document.addEventListener('keydown', function(event){
+    game.handleKeydown(event);
+});
